c5ffadb86f5bc7f49dffaa3b4aef9fa8e8714f13
Jorge Iraizoz <jorgeirai27@gmail.com>
2024-12-04 11:18:23 +0100

    Por la cara

--- js/voice-search.js
+++ js/voice-search.js
@@ -5,6 +5,7 @@ let microAceptado = false;
 const voiceSearchModalOpen = ()=>{
 	voiceSearch.style.display = "flex";
 	voiceSearch.style.animation = "aparecer 0.5s forwards";
+	console.log("Polla")
 	voiceRecognition();
 }
 
